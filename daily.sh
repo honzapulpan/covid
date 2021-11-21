758c95b7e92722f4a0ae12a602994ebe9dd23dfe
Honza Půlpán <honza@pulpan.net>
2021-11-21 08:08:08 +0100

    Update 2021-11-21

--- daily.sh
+++ daily.sh
@@ -4,7 +4,7 @@
 source /Users/honzapulpan/Projects/.venvs/jupyter/bin/activate
 
 cd /Users/honzapulpan/Projects/covid
-jupyter nbconvert --execute --to notebook covid19_stats.ipynb --output covid19_stats.ipynb
+jupyter nbconvert --execute --to notebook covid19_stats_api3.ipynb --output covid19_stats_api3.ipynb
 /Users/honzapulpan/Projects/covid/cmt.sh
 
 
